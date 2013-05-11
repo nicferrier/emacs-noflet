0bf743fef886b733bb20034becdaea2409ab918f
Nic Ferier <nic@ferrier.me.uk>
2013-05-11 19:47:23 +0100

    package header

--- noflet.el
+++ noflet.el
@@ -4,6 +4,8 @@
 
 ;; Author: Nic Ferrier <nferrier@ferrier.me.uk>
 ;; Keywords: lisp
+;; Version: 0.0.1
+;; Url: https://github.com/nicferrier/emacs-noflet
 
 ;; This program is free software; you can redistribute it and/or modify
 ;; it under the terms of the GNU General Public License as published by
