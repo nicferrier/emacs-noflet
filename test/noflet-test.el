790666cb1b899131a1da4503241d3ebe1d98051d
Ryan C. Thompson <rct@thompsonclan.org>
2017-05-30 16:52:52 -0400

    Add more tests

--- test/noflet-test.el
+++ test/noflet-test.el
@@ -7,9 +7,19 @@
 (ert-deftest noflet-test-simple ()
   :tags '(ido ido-ubiquitous)
   "Test that basic noflet functionality is working."
-  (should (= 3
-             (noflet ((1+ (x) (+ 1 x)))
-               (1+ 2)))))
+  (noflet ((1+ (x) (+ 1 x)))
+    (should (= 3 (1+ 2)))))
+
+(ert-deftest noflet-test-this-fn ()
+  "Test that `this-fn' can be used in noflet function bodies."
+  (noflet ((1+ (x) (funcall this-fn x)))
+    (should (= 3 (1+ 2)))))
+
+(ert-deftest noflet-test-letbind ()
+  "Test that let-bound variables are accessible inside function bodies."
+  (let ((myvar 5))
+    (noflet ((myfun (x) (1+ x)))
+      (myfun myvar))))
 
 (defun noflet-run-all-tests ()
   (interactive)
