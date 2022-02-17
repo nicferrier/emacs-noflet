e54b71d30022112e18f10a7c81ae3fc3321162a7
Jen-Chieh <jcs090218@gmail.com>
2022-02-17 14:08:36 +0800

    Add autoload

--- noflet.el
+++ noflet.el
@@ -116,6 +116,7 @@ name."
        (- (point)
           (line-beginning-position)))))
 
+;;;###autoload
 (defmacro noflet (bindings &rest body)
   "Make local function BINDINGS allowing access to the original.
 
@@ -145,6 +146,7 @@ points to `noflet|base' for all new bindings."
            (indent noflet-indent-func))
   (apply 'noflet|expand bindings body))
 
+;;;###autoload
 (defmacro nolexflet (bindings &rest body)
   "Lexical version.
 
